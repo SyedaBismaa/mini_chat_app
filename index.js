580fe0b0179becf862db8cbc4113ad760f68e0ae
SyedaBismaa <syedabisma173@gmail.com>
2025-07-16 15:15:10 +0530

    Ui || Time

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const http = require("http")
-const express = require("express")
-const path = require("path")
-const { Server } = require("socket.io")
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server)
+const io = new Server(server);
 
 app.use(express.static(path.resolve("./public")));
 
-app.get("/",(req,res)=>{
-  return res.sendFile("/public/index.html")
-})
-
-io.on('connection', (socket) => {
-
-    socket.on("user-message",(message)=>{
-        io.emit("message",message)
-    })
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve("./public/index.html"));
 });
- 
-server.listen(3000,()=>{
-    console.log("sever started at port 3000")
-})
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("user-message", ({ user, text }) => {
+    io.emit("message", { user, text, id: socket.id, time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) });
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
